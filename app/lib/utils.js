899227c27bac912ee8a0c46e1f1b6ebe424a9b32
Marco Tignanelli <93273771+Mark-kus@users.noreply.github.com>
2024-04-13 16:35:14 -0300

    add util for translating day to number

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -8,3 +8,17 @@ export const getColorByConfirmation = (confirmation) => {
       return "#CE9700";
   }
 };
+
+export const translateDay = (day, reverse) => {
+  const daysOfWeek = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miércoles",
+    "Jueves",
+    "Viernes",
+    "Sábado",
+  ];
+  if (reverse) return daysOfWeek.indexOf(day);
+  return daysOfWeek[day];
+};
