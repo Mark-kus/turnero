a92575351bfd155c15ccc1046500f5463ba2060f
Mark-kus <tignanellimarco@gmail.com>
2025-01-19 01:13:47 -0300

    Feat (Search Specialists): Add search params sync with inputs

--- app/appointment/page.js
+++ app/appointment/page.js
@@ -2,15 +2,35 @@ import React, { Suspense } from "react";
 import AppointmentForm from "../ui/forms/Appointment";
 import SpecialistList from "../ui/SpecialistList";
 import Modal from "../ui/modals/Confirmed";
-import { fetchInsurances, fetchSpecialties } from "../lib/data";
 import AppointmentsListSkeleton from "../ui/skeletons/SpecialistsListSkeleton";
+import Image from "next/image";
+import defaultImage from "@public/default/image.svg";
 
 const ProfessionalsSearch = async () => {
-  await fetchInsurances();
-  await fetchSpecialties();
   return (
     <main className="m-10 flex justify-between gap-10">
-      <AppointmentForm />
+      <section className="hero max-w-xl bg-neutral">
+        <div className="hero-content">
+          <div className="m-4 max-w-md">
+            <h1 className="text-2xl leading-3">Pick your appointment</h1>
+            <p className="py-4 text-xs">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo.
+            </p>
+            <div className="flex h-36 items-center justify-center rounded-lg border-2 border-base-300 bg-gray-200">
+              <Image
+                src={defaultImage}
+                alt="Default image"
+                width={20}
+                height={20}
+              />
+            </div>
+            <AppointmentForm />{" "}
+          </div>
+        </div>
+      </section>
       <Suspense fallback={<AppointmentsListSkeleton />}>
         <SpecialistList />
       </Suspense>
--- app/ui/forms/Appointment.jsx
+++ app/ui/forms/Appointment.jsx
@@ -1,59 +1,61 @@
-import Image from "next/image";
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
-import defaultImage from "@public/default/image.svg";
-
 const AppointmentForm = () => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleChange = ({ target: { name, value } }) => {
+    const params = new URLSearchParams(searchParams);
+
+    if (value) {
+      params.set(name, value);
+    } else {
+      params.delete(name);
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+  };
+
   return (
-    <section className="hero max-w-xl bg-neutral">
-      <div className="hero-content">
-        <div className="m-4 max-w-md">
-          <h1 className="text-2xl leading-3">Pick your appointment</h1>
-          <p className="py-4 text-xs">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo.
-          </p>
-          <div className="flex h-36 items-center justify-center rounded-lg border-2 border-base-300 bg-gray-200">
-            <Image
-              src={defaultImage}
-              alt="Default image"
-              width={20}
-              height={20}
-            />
-          </div>
-          <form>
-            <label className="form-control mt-6 w-full">
-              <span className="my-2 text-xs opacity-50">Specialty</span>
-              <input
-                className="input-minimal"
-                placeholder="Placeholder/Input text"
-              />
-            </label>
-            <label className="form-control mt-4 w-full">
-              <span className="my-2 text-xs opacity-50">
-                Doctor&apos;s name
-              </span>
-              <input
-                className="input-minimal"
-                placeholder="Placeholder/Input text"
-              />
-            </label>
-            <label className="form-control mt-4 w-full">
-              <span className="my-2 text-xs opacity-50">Health insurance</span>
-              <input
-                className="input-minimal"
-                placeholder="Placeholder/Input text"
-              />
-            </label>
-            <button className="btn-round btn-h-10 btn btn-primary mt-6 w-full text-xs font-medium">
-              Label
-            </button>
-          </form>
-        </div>
-      </div>
-    </section>
+    <form>
+      <label className="form-control mt-6 w-full">
+        <span className="my-2 text-xs opacity-50">Specialty</span>
+        <input
+          name="specialty"
+          defaultValue={searchParams.get("specialty")?.toString()}
+          onChange={handleChange}
+          className="input-minimal"
+          placeholder="Placeholder/Input text"
+        />
+      </label>
+      <label className="form-control mt-4 w-full">
+        <span className="my-2 text-xs opacity-50">Doctor&apos;s name</span>
+        <input
+          name="name"
+          defaultValue={searchParams.get("name")?.toString()}
+          onChange={handleChange}
+          className="input-minimal"
+          placeholder="Placeholder/Input text"
+        />
+      </label>
+      <label className="form-control mt-4 w-full">
+        <span className="my-2 text-xs opacity-50">Health insurance</span>
+        <input
+          name="insurance"
+          defaultValue={searchParams.get("insurance")?.toString()}
+          onChange={handleChange}
+          className="input-minimal"
+          placeholder="Placeholder/Input text"
+        />
+      </label>
+      <button className="btn-round btn-h-10 btn btn-primary mt-6 w-full text-xs font-medium">
+        Label
+      </button>
+    </form>
   );
 };
 
